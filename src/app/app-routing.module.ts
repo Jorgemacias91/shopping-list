7fdcbd135bb6b25c8262d9730237e7376bef4a5e
jorge <jmaciasvesga@gmail.com>
2021-04-18 18:07:52 -0500

    falta solo responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { AdditemComponent } from './components/additem/additem.component';
+import { ItemsComponent } from './components/items/items.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:ItemsComponent
+  },
+  {
+    path:'add',
+    component:AdditemComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
